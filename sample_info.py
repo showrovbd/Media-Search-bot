e270d1f60396c2a1358a2856a51dbf24e14f2808
showrovbd <115853411+showrovbd@users.noreply.github.com>
2024-01-11 23:14:12 +0600

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 15799842
+API_HASH = 'feef551257bd1d00f36854fe29e98d96'
+BOT_TOKEN = '6827678309:AAFTq6BU1nn8Wx5iW3dGXPQmfMlAR2MMU58'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,22 +11,21 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1230149617]
+CHANNELS = [-1002113918383]
 AUTH_USERS = []
-AUTH_CHANNEL = None
+AUTH_CHANNEL = -1002113918383
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://montouch69:montouch69@cluster0.kwz2rrn.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I'm Resource Helper of Design Automation**
 
-Here you can search files in inline mode. Just press follwing buttons and start searching.
-"""
+Here you can search files in inline mode. Just press following buttons and start searching.
 
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = 'Please join @.... to use this bot'
+SHARE_BUTTON_TEXT = 'Checkout {@DAresourceHelper_bot} for searching files'
+INVITE_MSG = 'Please join @designautomation24 to use this bot'
